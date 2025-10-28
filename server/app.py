df0a8d78c0fb9cbdc086e0d21c175db08295ee6b
Dawid Kacza <dawid.kacza@sap.com>
2025-10-28 20:56:14 +0100

    Added serving static files from the BE

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 # server/app.py
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from routes.auth_routes import auth_bp
@@ -9,6 +9,7 @@ from sockets.game_events import register_socket_events
 from config import FRONTEND_URL, SERVER_PORT, CERT_PATH, KEY_PATH
 from sockets.socket import socketio
 import eventlet
+import os
 
 app = Flask(__name__)
 
@@ -27,6 +28,15 @@ CORS(
     supports_credentials=True,
     origins=[FRONTEND_URL],
 )
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve_react(path):
+    static_dir = os.path.join(app.root_path, "static")
+    file_path = os.path.join(static_dir, path)
+    if path and os.path.exists(file_path):
+        return send_from_directory(static_dir, path)
+    return send_from_directory(static_dir, "index.html")
 
 if __name__ == "__main__":
     listener = eventlet.listen(("127.0.0.1", SERVER_PORT))
