769cffa8f997531c9fa41a1f262b6b91a95db8b3
Dawid Kacza <dawid.kacza@sap.com>
2025-10-31 16:13:26 +0100

    Refactored Authentication Context

--- client/src/providers/AuthProvider.jsx
+++ client/src/providers/AuthProvider.jsx
@@ -1,11 +1,33 @@
-import { createContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import NotificationContext, {
+  CustomNotification,
+} from "./NotificationProvider";
+// @ts-ignore
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-const AuthContext = createContext({});
+const AuthContext = createContext({
+  user: {},
+  loading: true,
+  token: null,
+  refreshUser: async () => {},
+});
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [token, setToken] = useState(null);
+
+  // UseEffect double execution prevention
+  const executedRef = useRef(false);
+
+  const { addNotification } = useContext(NotificationContext);
 
   // After the spotify callback is served, the redirection to the FE is made.
   // The sid parameter passed within the URL query can be used to retrieve the specific Spotify Web API token from the BE
@@ -14,66 +36,103 @@ export function AuthProvider({ children }) {
     return params.get("sid");
   };
 
-  async function getSpotifyTokenForProcessing() {
+  // Exchange sid request param for HttpOnly cookie with Spotify API token
+  const exchangeSessionForToken = useCallback(
+    async (sid) => {
+      try {
+        const res = await fetch(`${BACKEND_URL}/api/set-api-token`, {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ session_id: sid }),
+        });
+        if (!res.ok) throw new Error("Failed to exchange Spotify token");
+
+        addNotification(
+          new CustomNotification("success", "Spotify login successful")
+        );
+      } catch (err) {
+        console.error(err);
+        addNotification(new CustomNotification("error", err.message));
+      }
+    },
+    [addNotification]
+  );
+
+  // Read the HTTP only Spotify API key cookie and return it within the response body
+  const fetchSpotifyToken = useCallback(async () => {
     try {
-      const res = await fetch("https://localhost:5000/api/spotify-token", {
+      const res = await fetch(`${BACKEND_URL}/api/spotify-token`, {
         credentials: "include",
       });
-      if (!res.ok) throw new Error("Failed to retrieve Spotify token");
+      if (!res.ok) {
+        throw new Error();
+      }
       const { token } = await res.json();
       return token;
-    } catch (err) {
-      console.error(err);
+    } catch {
+      const errorMsg = "Failed to retrieve Spotify token";
+      addNotification(new CustomNotification("error", errorMsg));
       return null;
     }
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  const getSpotifyToken = async (sid) => {
-    const res = await fetch("https://localhost:5000/api/set-api-token", {
-      method: "POST",
-      credentials: "include",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ session_id: sid }),
-    });
-    if (!res.ok) throw new Error("Failed to set Spotify Web API token");
-  };
-
-  const refreshUser = async () => {
+  /** Load current Spotify user profile */
+  const refreshUser = useCallback(async () => {
+    setLoading(true);
     try {
-      const res = await fetch("https://localhost:5000/api/me", {
+      const res = await fetch(`${BACKEND_URL}/api/me`, {
         credentials: "include",
       });
-
-      if (!res.ok) {
-        throw new Error("Not authenticated");
-      }
+      if (!res.ok) throw new Error("Unauthenticated");
 
       const data = await res.json();
       setUser(data);
 
-      const tokenInJS = await getSpotifyTokenForProcessing();
-      setToken(tokenInJS);
-    } catch {
+      const jsToken = await fetchSpotifyToken();
+      setToken(jsToken);
+    } catch (err) {
       setUser(null);
+      setToken(null);
+      addNotification(new CustomNotification("error", err.message));
     } finally {
       setLoading(false);
     }
-  };
+  }, [addNotification, fetchSpotifyToken]);
 
   useEffect(() => {
+    if (executedRef.current) return;
+    executedRef.current = true;
     (async () => {
-      const sessionIdFromUrl = retrieveSessionIdfromURL();
-      if (sessionIdFromUrl) {
-        await getSpotifyToken(sessionIdFromUrl);
-        window.history.replaceState(
-          {},
-          document.title,
-          window.location.pathname
+      try {
+        const sid = retrieveSessionIdfromURL();
+        if (sid) {
+          console.log("[AUTH] Found session ID from redirect:", sid);
+          await exchangeSessionForToken(sid);
+          // remove sid from URL
+          window.history.replaceState(
+            {},
+            document.title,
+            window.location.pathname
+          );
+        }
+        await refreshUser();
+      } catch (err) {
+        console.error("[AUTH] Error when refreshing user", err);
+        addNotification(
+          new CustomNotification(
+            "error",
+            "An error occured when refreshing user"
+          )
         );
+
+        setUser(null);
+        setToken(null);
+        setLoading(false);
       }
-      await refreshUser();
     })();
-  }, []);
+  }, [addNotification, exchangeSessionForToken, refreshUser]);
 
   return (
     <AuthContext.Provider value={{ user, loading, token, refreshUser }}>
