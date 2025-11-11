064d52c2261ebeab2a847eba460049641e3f67af
Dawid Kacza <dawid.kacza@sap.com>
2025-11-11 11:10:22 +0100

    Fixed vite configuration to omit certs on prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import fs from "fs";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync("./localhost+2-key.pem"),
-      cert: fs.readFileSync("./localhost+2.pem"),
-    },
-    port: 5173,
-    proxy: {
-      "/auth": "https://localhost:5000",
-      "/api": "https://localhost:5000",
-    },
-  }
-})
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === "development";
+
+  return {
+    plugins: [react()],
+    server: isDevelopment
+      ? {
+          https: {
+            key: fs.readFileSync("./localhost+2-key.pem"),
+            cert: fs.readFileSync("./localhost+2.pem"),
+          },
+          port: 5173,
+          proxy: {
+            "/auth": "https://localhost:5000",
+            "/api": "https://localhost:5000",
+          },
+        }
+      : { port: 5173 },
+  };
+});
